a1da0f9a15c41207a3be22a2da890fb20a7dfceb
Brady Shober <shoberb@gmail.com>
2015-05-05 20:22:39 -0400

    Add /website command

--- CephalInfo/src/main/java/com/github/butter1484/CephalInfo/CephalInfo.java
+++ CephalInfo/src/main/java/com/github/butter1484/CephalInfo/CephalInfo.java
@@ -1,5 +1,9 @@
 package com.github.butter1484.CephalInfo;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -10,6 +14,18 @@ public void onEnable(){
 }
 
 public void onDisable(){
+	
+}
+
+public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+	Player player = (Player) sender;
+	if(cmd.getName().equalsIgnoreCase("website") && player instanceof Player){
+		player.sendMessage(ChatColor.GREEN + "Check out our website at " + ChatColor.YELLOW + "http://cephal.enjin.com/");
+		return true;
+	}
+	
+	
+	return false;
 	
 }
 }
